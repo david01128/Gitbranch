86c331c6461572c6bd2b87d3b0ad067a49ead99f
davidshyu <david005611@gmail.com>
2022-02-10 09:35:23 +0800

    1st Version

---
+++ GitBranchSample/src/GitBranchApp/MainApp.java
@@ -0,0 +1,12 @@
+package GitBranchApp;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//組長第一次編輯
+		System.out.println("1st version");
+
+	}
+
+}
